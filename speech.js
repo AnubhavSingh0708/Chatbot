f9381e48bc8f60fe8a342fd6d1468bb55c3de00c
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-03-02 22:34:51 +0530

    Update speech.js

--- speech.js
+++ speech.js
@@ -3,14 +3,14 @@
 const synth = window.speechSynthesis;
 
 const textToSpeech = (string) => {
-  let voice = new SpeechSynthesisUtterance(string);
+  let voce = new SpeechSynthesisUtterance(string);
   var voices = window.speechSynthesis.getVoices();
 
-voice.voice = voices[0];
-  voice.text = string;
-  voice.lang = "en-US";
-  voice.volume = 1;
-  voice.rate = 1;
-  voice.pitch = 1; // Can be 0, 1, or 2
-  synth.speak(voice);
+voce.voice = voices[0];
+  voce.text = string;
+  voce.lang = "en-US";
+  voce.volume = 1;
+  voce.rate = 1;
+  voce.pitch = 1; // Can be 0, 1, or 2
+  synth.speak(voce);
 }
